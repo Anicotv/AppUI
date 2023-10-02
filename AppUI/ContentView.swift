ad150661c5c79dc84888b0343c859630c7aed0e4
Anissa <anicoletrevino@gmail.com>
2023-10-02 14:47:16 -0500

    Testing git pull and commit repository access

--- AppUI/ContentView.swift
+++ AppUI/ContentView.swift
@@ -6,16 +6,78 @@
 //
 
 import SwiftUI
+import AVKit
+import AVFoundation
+
+
+struct PlayerView: UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        return LoopingPlayerUIView(frame: .zero)
+    }
+}
+
+
+class LoopingPlayerUIView: UIView {
+    private let playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+
+
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        // Load the resource
+        let fileUrl = Bundle.main.url(forResource: "splashanim", withExtension: "mov")!
+        let asset = AVAsset(url: fileUrl)
+        let item = AVPlayerItem(asset: asset)
+
+        // Setup the player
+        let player = AVQueuePlayer()
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+
+        // Create a new player looper with the queue player and template item
+        playerLooper = AVPlayerLooper(player: player, templateItem: item)
+
+        // Start the movie
+        player.play()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+}
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        GeometryReader{ geo in
+            ZStack {
+                PlayerView()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width, height: geo.size.height+100)
+                .edgesIgnoringSafeArea(.all)
+
+                VStack{
+                    Text("Hello World")
+                        .font(.title)
+                        .foregroundColor(.white)
+                    Spacer()
+                }
+            }
         }
-        .padding()
+
     }
 }
 
