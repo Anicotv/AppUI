429f4a0d64fbe02b749dadc9384ab66b8bde47aa
Anissa <anicoletrevino@gmail.com>
2023-10-02 14:02:21 -0500

    Initial Commit

---
+++ AppUI/AppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppUIApp.swift
+//  AppUI
+//
+//  Created by Anissa on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
